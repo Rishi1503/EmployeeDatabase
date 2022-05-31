0f3c8c758da9585280231302fc5aba76ff1f441c
Rishit155 <89496208+Rishit155@users.noreply.github.com>
2022-05-31 15:54:59 -0400

    Add files via upload

---
+++ vue-3-crud/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <router-link to="/employees" class="navbar-brand">Employee App</router-link>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link to="/employees" class="nav-link">Employees</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/add" class="nav-link">Add Employee</router-link>
+        </li>
+      </div>
+    </nav>
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app"
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
